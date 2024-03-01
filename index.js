770b740610fc904ba7661b4490a9ff53b27ef769
MiloKow <miloperso.kowalska@gmail.com>
2024-03-01 11:02:28 +0100

    :fire: [Add] Adding the product

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
   main();
+  const divElements = document.querySelectorAll('.article');
+  divElements.forEach(function(divElement) {
+    divElement.addEventListener('click', function() {
+      const destination = divElement.getAttribute('dest');
+      if (destination) {
+        window.location.href = destination;
+      }
+    });
+  });
 });
 async function main() {
   try {
@@ -7,18 +16,35 @@ async function main() {
       method: 'GET',
       url: 'https://api.kedufront.juniortaker.com/item/',
     });
-    console.log(response.data)
     const items = response.data;
     const nameElements = document.querySelectorAll('.name');
     const priceElements = document.querySelectorAll('.price');
+    const imgElements = document.querySelectorAll('.image');
 
-    items.forEach((item, index) => {
-      if (nameElements[index]) {
-        nameElements[index].textContent = item.name;
-        priceElements[index].textContent = item.price;
+    for (let i = 0; i < 6; i++) {
+      const item = items[i];
+      if (nameElements[i]) {
+        nameElements[i].textContent = item.name;
+        priceElements[i].textContent = item.price;
       }
-    });
+      const imageUrl = await getImageById(item._id);
+      imgElements[i].src = imageUrl;
+      imgElements[i].alt = item.name;
+    }
   } catch(error) {
     console.error('Une erreur s\'est produite :', error);
+  }
+}
+
+async function getImageById(id) {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: `https://api.kedufront.juniortaker.com/item/picture/${id}`,
+    });
+    return response.config.url;
+  } catch (error) {
+    console.error('Une erreur s\'est produite lors de la récupération de l\'image :', error);
+    return null;
   }
 }
